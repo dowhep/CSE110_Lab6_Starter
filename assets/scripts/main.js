f69d0891d0f07169e387c81c2c5d3ba21ffd67f9
Marcelo Shen <caogangshen@gmail.com>
2023-05-14 22:26:22 -0700

    expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,9 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  let recipesRaw = localStorage.getItem('recipes');
+  if (!recipesRaw) return [];
+  return JSON.parse(recipesRaw);
 }
 
 /**
@@ -35,10 +38,17 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  let mainEl = document.querySelector('main');
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  for (const recipe of recipes) {
+    let recipeCardEl = document.createElement('recipe-card');
+    // console.log(recipe);
+    recipeCardEl.data = recipe;
+    mainEl.append(recipeCardEl);
+  }
 }
 
 /**
